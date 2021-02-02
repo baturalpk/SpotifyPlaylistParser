7757a37365f8ba6889add3121399b388f06ade57
baturalp-kiziltan <baturalp.kiziltan@outlook.com>
2021-02-02 04:57:53 +0300

    🔨 Build up 'playlist parser'

--- parser.js
+++ parser.js
@@ -1,24 +1,44 @@
 const fs = require('fs')
 
 const PlaylistParser = (data, type) => {
-    var result = '';
     if (type === 'Playlist') {
-        result += `PLAYLIST: ${data.name}\n`;
-        result += `Owner: ${data.owner.name}\n`;
-        result += `Number of songs: ${data.tracks.total}\n`;
-        result += `TRACKS:\n`;
-        
-        for (var i = 0; i < data.tracks.total; ++i)
-            result += `\t${i+1}. ${data.tracks.items[i].track.name}`;
+        var parsedData = {
+            "name": data.name,
+            "owner": data.owner.display_name,
+            "length": data.tracks.total,
+            "tracks": []
+        }
 
+        for (var i = 0; i < parsedData.length; ++i) {
+
+            if (i == 100) break;
+
+            var trackInfo = {
+                "track": {
+                    "name": data.tracks.items[i].track.name,
+                    "artists": {}
+                }
+            }
+
+            var artists = '';
+            for (var j = 0; j < data.tracks.items[i].track.artists.length; ++j) {
+                if (j != 0)
+                    artists += ' & ' + data.tracks.items[i].track.artists[j].name;
+                else
+                    artists += data.tracks.items[i].track.artists[j].name;
+            }
+            
+            trackInfo.track.artists = artists;
+            parsedData.tracks.push(trackInfo);
+        }
+            
         ClearTempFile();
-        console.log(result);
-        return result;
+        return parsedData;
     }
     else if (type === 'MP3') {
         // TODO: play or download preview music
+        return parsedData;
     }
-
     else 
         console.log('JsonParser: wrong type decleration!');
 }
